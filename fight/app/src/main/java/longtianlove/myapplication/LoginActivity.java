b4cbaafc4f02aa3f4c93a965512d578e288a2acb
王彬龙 <wangbinlong@58ganji.com>
2016-12-11 23:11:05 +0800

    初始化并提交忽略文件

---
+++ fight/app/src/main/java/longtianlove/myapplication/LoginActivity.java
@@ -0,0 +1,17 @@
+package longtianlove.myapplication;
+
+import android.os.Bundle;
+
+import longtianlove.bottomlib.base.BaseActivity;
+
+/**
+ * Created by long on 2016/12/11.
+ */
+
+public class LoginActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ fight/bottomlib/src/main/java/longtianlove/bottomlib/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package longtianlove.bottomlib.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by long on 2016/12/11.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
