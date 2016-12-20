d9fb48d9b6bc8d1cead39be5dedd61bbe2ff24bc
王彬龙 <wangbinlong@58ganji.com>
2016-12-20 23:11:09 +0800

    部分提交

---
+++ fight/containerbus/src/main/java/container/eventbus/EventBus.java
@@ -0,0 +1,22 @@
+package container.eventbus;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by 58 on 2016/12/20.
+ */
+
+public class EventBus {
+    public void register(Object subsciber) {
+        //从subscriber获取以onEvent开头的method
+        Class subscriberClass = subsciber.getClass();
+       Method[] subscriberMethod= subscriberClass.getMethods();
+        for(Method method:subscriberMethod){
+
+        }
+    }
+    subscribe(Object ){
+
+    }
+
+}
